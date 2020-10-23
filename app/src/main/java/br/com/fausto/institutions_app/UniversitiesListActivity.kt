2b0a68412536594e14f2713652a6f86504456289
Fausto Neves Kina <fausto.kina@cromai.com>
2020-10-23 17:12:06 -0300

    Adiciona o mesmo item 500 vezes

--- app/src/main/java/br/com/fausto/institutions_app/UniversitiesListActivity.kt
+++ app/src/main/java/br/com/fausto/institutions_app/UniversitiesListActivity.kt
@@ -1,42 +1,28 @@
 package br.com.fausto.institutions_app
 
+import android.content.Intent
+import android.net.Uri
 import android.os.Bundle
-import android.util.Log
 import androidx.appcompat.app.AppCompatActivity
 import androidx.recyclerview.widget.LinearLayoutManager
 import br.com.fausto.institutions_app.model.UniversityParsed
 import br.com.fausto.institutions_app.model.UniversityParsedItem
 import kotlinx.android.synthetic.main.activity_list_universities.*
 
-
 class UniversitiesListActivity : AppCompatActivity(), UniversityAdapter.OnUniversityListener {
-//    lateinit var viewModel: UniversityViewModel
 
+    private lateinit var arrayParseado: ArrayList<UniversityParsedItem>
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_list_universities)
-
         val intent = intent
         val list: ArrayList<UniversityParsed> = intent.getSerializableExtra("list") as ArrayList<UniversityParsed>
+        val list2 = list.toArray()
+        arrayParseado = ArrayList()
 
-        val list1 = list.toArray()
-        val list2: Array<UniversityParsedItem>
-        val arrayParseado = ArrayList<UniversityParsedItem>()
-
-        val listaFinal: List<UniversityParsedItem>
-        val arrayFinal: Array<UniversityParsedItem>
-
-        listaFinal = arrayParseado.toList()
-//        arrayFinal = arrayParseado.toArray() as Array<UniversityParsedItem>
-
-        for (i in list1) {
+        for (i in list2) {
             arrayParseado.add(i as UniversityParsedItem)
-            Log.e("MORGAN FREEMAN", i.name.toString())
         }
-
-//        for (e in listaFinal) {
-//            Log.e("DENZEL WASHINGTON", e.name.toString())
-//        }
 
         recyclerView.adapter = UniversityAdapter(arrayParseado, this, this)
         recyclerView.layoutManager = LinearLayoutManager(this, LinearLayoutManager.VERTICAL, false)
@@ -44,11 +30,11 @@ class UniversitiesListActivity : AppCompatActivity(), UniversityAdapter.OnUniver
     }
 
     override fun onUniversityClick(position: Int) {
-//        val universityItem = list[position]
-//        val uri = Uri.parse(universityItem.web_pages!![0])
-//        val intent = Intent(Intent.ACTION_VIEW, uri)
-//        if (intent.resolveActivity(packageManager) != null) {
-//            startActivity(intent)
-//        }
+        val universityItem = arrayParseado[position]
+        val uri = Uri.parse(universityItem.web_pages!![0])
+        val intent = Intent(Intent.ACTION_VIEW, uri)
+        if (intent.resolveActivity(packageManager) != null) {
+            startActivity(intent)
+        }
     }
 }
